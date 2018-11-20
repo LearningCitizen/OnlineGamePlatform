da212ea04909878312feced0c6bc9fb0e949dc2a
LearningCitizen <johnson.hippolyte@yahoo.fr>
2018-11-20 15:01:09 +0100

    creation of the project with spring initializr

---
+++ src/main/java/com/gamesplatform/www/GamesPlatform/GamesPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.gamesplatform.www.GamesPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesPlatformApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gamesplatform/www/GamesPlatform/GamesPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gamesplatform.www.GamesPlatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamesPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
